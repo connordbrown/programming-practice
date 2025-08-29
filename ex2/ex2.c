9165001188e6aa650e94f7c4cd4214e585ddfaa3
Connor <connordbrown@gmail.com>
2025-08-29 15:58:32 -0600

    complete ex2

---
+++ ex2/ex2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    // buffer for getline
+    char *line = NULL;
+    // getline buffer size
+    size_t len = 0;
+    // number of characters read by getline
+    ssize_t read;
+
+    // get first number
+    printf("%s", "Enter first number: ");
+    read = getline(&line, &len, stdin);
+    if (read == -1) {
+        perror("getline");
+        free(line);
+        return EXIT_FAILURE;
+    }
+    int num1 = atoi(line);
+    
+    // get second number
+    printf("%s", "Enter second number: ");
+    read = getline(&line, &len, stdin);
+    if (read == -1) {
+        perror("getline");
+        free(line);
+        return EXIT_FAILURE;
+    }
+    int num2 = atoi(line);
+
+    // print sum
+    printf("Sum: %d\n", num1 + num2);
+
+    free(line);
+    return EXIT_SUCCESS;
+}
