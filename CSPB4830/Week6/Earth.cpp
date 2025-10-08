11b9f0f8634e0977babb51ea880bcb5ef2e498b6
Connor <connordbrown@gmail.com>
2025-10-08 15:00:04 -0600

    add Makefile for activity

---
+++ CSPB4830/Week6/Earth.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Earth.h"
+
+const int Earth::NUM_CONTINENTS = 7;
+
+Earth::Earth()
+{
+  // Implement your constructor here
+  // make sure to initialize your fields!
+  this->population_ = 0;
+}
