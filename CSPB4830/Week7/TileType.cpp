d6b137bd7540cedcba12ed8c1f94c9cc968b0193
Connor <connordbrown@gmail.com>
2025-10-14 16:33:18 -0600

    add some TileType methods

--- CSPB4830/Week7/TileType.cpp
+++ CSPB4830/Week7/TileType.cpp
@@ -3,3 +3,16 @@
 #include "TileType.h"
 
 // 4) implement methods
+TileType::TileType(std::string emoji, bool walkable, bool dangerous): emoji_(emoji), walkable_(walkable), dangerous_(dangerous) {}
+
+std::string TileType::get_emoji() {
+    return this->emoji_;
+}
+
+bool TileType::get_walkable() {
+    return this->walkable_;
+}
+
+bool TileType::get_dangerous() {
+    return this->dangerous_;
+}
